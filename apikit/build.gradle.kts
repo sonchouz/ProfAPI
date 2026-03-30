fc274a6064a878c0286dc61f7b5bc40995c25b43
sonchouz <sofianikonorova41@gmail.com>
2026-03-30 12:21:27 +0700

    fixed bugs

--- apikit/build.gradle.kts
+++ apikit/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 
 }
 
@@ -29,6 +30,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
@@ -45,4 +51,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.sonchouz"
+                artifactId = "ProfAPI"
+                version = "1.0"
+            }
+        }
+    }
 }
